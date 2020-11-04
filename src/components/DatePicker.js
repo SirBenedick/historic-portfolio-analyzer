5ffce3538cd13c6b200e8fea86b50bdd8b17c2dc
SirBenedick <beneweb@outlook.de>
2020-11-04 20:21:31 +0100

    Clean up

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,37 +1,30 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
 import { observer } from "mobx-react";
-import MomentUtils from '@date-io/moment';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+import MomentUtils from "@date-io/moment";
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
 
-const DatePicker = observer(({dataStore})=>{
-
+const DatePicker = observer(({ dataStore }) => {
   const handleDateChange = (date) => {
-    dataStore.setPortfolioStartingDate(date.format("YYYY-MM-DD"))
+    dataStore.setPortfolioStartingDate(date.format("YYYY-MM-DD"));
   };
 
   return (
     <MuiPickersUtilsProvider utils={MomentUtils}>
-      <Grid container justify="space-around">
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="yyyy-MM-DD"
-          margin="normal"
-          id="date-picker-starting-datet"
-          label="Starting date"
-          value={dataStore.portfolioStartingDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-      </Grid>
+      <KeyboardDatePicker
+        disableToolbar
+        variant="inline"
+        format="yyyy-MM-DD"
+        margin="normal"
+        id="date-picker-starting-datet"
+        label="Starting date"
+        value={dataStore.portfolioStartingDate}
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          "aria-label": "change date",
+        }}
+      />
     </MuiPickersUtilsProvider>
   );
-})
+});
 
 export default DatePicker;
