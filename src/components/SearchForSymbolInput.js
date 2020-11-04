3f3e7cd9ea7021c6a2d6202ca444938d93404754
SirBenedick <beneweb@outlook.de>
2020-11-04 17:37:48 +0100

    Improve UX

--- src/components/SearchForSymbolInput.js
+++ src/components/SearchForSymbolInput.js
@@ -41,7 +41,7 @@ const SearchForSymbolInput = observer(({ dataStore }) => {
     if (!keywords) return;
     const timeout = setTimeout(async () => {
       await handleSearch(keywords);
-    }, 1000);
+    }, 500);
     setSearchTimeout(timeout);
   };
 
@@ -77,74 +77,3 @@ const SearchForSymbolInput = observer(({ dataStore }) => {
   );
 });
 export default SearchForSymbolInput;
-
-// const example_res = {
-//   bestMatches: [
-//     {
-//       "1. symbol": "BA",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "United States",
-//       "5. marketOpen": "09:30",
-//       "6. marketClose": "16:00",
-//       "7. timezone": "UTC-05",
-//       "8. currency": "USD",
-//       "9. matchScore": "0.7273",
-//     },
-//     {
-//       "1. symbol": "BCO.DEX",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "XETRA",
-//       "5. marketOpen": "08:00",
-//       "6. marketClose": "20:00",
-//       "7. timezone": "UTC+01",
-//       "8. currency": "EUR",
-//       "9. matchScore": "0.6667",
-//     },
-//     {
-//       "1. symbol": "BOE.LON",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "United Kingdom",
-//       "5. marketOpen": "08:00",
-//       "6. marketClose": "16:30",
-//       "7. timezone": "UTC+00",
-//       "8. currency": "GBP",
-//       "9. matchScore": "0.6154",
-//     },
-//     {
-//       "1. symbol": "BOEI34.SAO",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "Brazil/Sao Paolo",
-//       "5. marketOpen": "10:00",
-//       "6. marketClose": "17:30",
-//       "7. timezone": "UTC-03",
-//       "8. currency": "BRL",
-//       "9. matchScore": "0.6000",
-//     },
-//     {
-//       "1. symbol": "BCO.FRK",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "Frankfurt",
-//       "5. marketOpen": "08:00",
-//       "6. marketClose": "20:00",
-//       "7. timezone": "UTC+01",
-//       "8. currency": "EUR",
-//       "9. matchScore": "0.4444",
-//     },
-//     {
-//       "1. symbol": "BOEI.BRU",
-//       "2. name": "The Boeing Company",
-//       "3. type": "Equity",
-//       "4. region": "Brussels",
-//       "5. marketOpen": "09:00",
-//       "6. marketClose": "17:00",
-//       "7. timezone": "UTC+01",
-//       "8. currency": "EUR",
-//       "9. matchScore": "0.3636",
-//     },
-//   ],
-// };
