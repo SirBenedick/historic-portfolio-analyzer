6c71a9cd9bb96724bf059b80ebc0fd5e45e577c8
SirBenedick <beneweb@outlook.de>
2020-11-06 11:54:22 +0100

    Add API-Token input

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { Paper, Typography } from "@material-ui/core";
+import { Paper, Typography, TextField, Link } from "@material-ui/core";
 
 const SettingsPage = ({ dataStore }) => {
+  const preventDefault = (event) => event.preventDefault();
   return (
     <div>
-      <Paper elevation={3}>
-        <Typography>Hello</Typography>
+      <Paper elevation={1} style={{ padding: "20px" }}>
+        <Typography variant="h1" gutterBottom>
+          Settings
+        </Typography>
+
+        <Typography variant="h6" gutterBottom>
+          Alpha Vantag API Token
+        </Typography>
+        <TextField id="outlined-basic" label="API-Token" variant="outlined" />
+        <Typography variant="caption" display="block" gutterBottom>
+          Get your free token here:
+          <Link href="https://www.alphavantage.co/support/#api-key" target="_blank" rel="noreferrer">
+            Alpha Vantage
+          </Link>
+        </Typography>
       </Paper>
     </div>
   );
