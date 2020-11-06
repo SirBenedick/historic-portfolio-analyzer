007bc5d295d14aedb593cc8f486a0e686ea546a9
SirBenedick <beneweb@outlook.de>
2020-11-06 18:29:43 +0100

    Create API-Token

--- src/stores/ConfigStore.js
+++ src/stores/ConfigStore.js
@@ -7,20 +7,21 @@ class ConfigStore {
   alphaVantageConstants = { SYMBOL_SEARCH: "SYMBOL_SEARCH", TIME_SERIES_DAILY_ADJUSTED: "TIME_SERIES_DAILY_ADJUSTED" };
 
   constructor() {
-    // Load stored API token from idbConfigStore
-    idbConfigStore.get("alphaVantagAPIToken").then((token) => this.setAlphaVantageAPIToken(token));
+    // Load stored API token from idbConfigStore or create a Token
+    idbConfigStore.get("alphaVantagAPIToken").then((token) => {
+      if (token) this.setAlphaVantageAPITokenHelper(token);
+      else {
+        this.setAlphaVantageAPITokenIDB(fakeToken(16));
+      }
+    });
 
     makeObservable(this, {
       alphaVantage: observable,
-      setAlphaVantageAPIToken: action,
+      setAlphaVantageAPITokenHelper: action,
     });
   }
 
-  setAlphaVantageAPIToken(newToken) {
-    this.alphaVantage.apiToken = newToken;
-  }
-
-  // Store new alphaVantagAPIToken to idbConfigStore
+  // Store new alphaVantagAPIToken to idbConfigStore then store in mobx store
   async setAlphaVantageAPITokenIDB(newToken) {
     await idbConfigStore.set("alphaVantagAPIToken", newToken);
     notificationStore.enqueueSnackbar({
@@ -31,8 +32,22 @@ class ConfigStore {
       },
       key: notificationStore.keys.API_TOKEN_STORED,
     });
-    this.setAlphaVantageAPIToken(newToken);
+    this.setAlphaVantageAPITokenHelper(newToken);
   }
+
+  setAlphaVantageAPITokenHelper(newToken) {
+    this.alphaVantage.apiToken = newToken;
+  }
+}
+
+function fakeToken(length) {
+  let result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 const configStore = new ConfigStore();
