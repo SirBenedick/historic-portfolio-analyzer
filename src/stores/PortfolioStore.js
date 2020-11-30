deb4c9c62f297ab193407b521655240b9e8f03da
SirBenedick <beneweb@outlook.de>
2020-11-30 15:50:47 +0100

    Refactor chatcolors into its own object

--- src/stores/PortfolioStore.js
+++ src/stores/PortfolioStore.js
@@ -12,7 +12,7 @@ class PortfolioStore {
       currency: "USD",
       performanceSincePortfolioStart: 1,
       annualizedPerformanceSincePortfolioStart: 1,
-      color: this.nextAvailableColorValue(),
+      color: chartColors.nextAvailableColorValue(),
       endValue: 0,
       dateFetched: "-",
     },
@@ -98,7 +98,7 @@ class PortfolioStore {
       value: 100,
       endValue: 0,
       totalDividendPayout: 0,
-      color: this.nextAvailableColorValue(),
+      color: chartColors.nextAvailableColorValue(),
       dateFetched: "-",
     });
 
@@ -123,7 +123,7 @@ class PortfolioStore {
   }
 
   async removeSelectedSymbol(symbolTickerToRemove) {
-    this.removeColorInUse(this.getSymbolSetForTicker(symbolTickerToRemove).color);
+    chartColors.removeColorInUse(this.getSymbolSetForTicker(symbolTickerToRemove).color);
     this.symbols = this.symbols.filter((symbolSet) => symbolSet.symbolTicker !== symbolTickerToRemove);
 
     await symbolDataStore.removeSymbolFromMap(symbolTickerToRemove);
@@ -138,7 +138,7 @@ class PortfolioStore {
 
   async resetStore() {
     // Clear colors
-    this.symbols.forEach((symbolSet) => this.removeColorInUse(symbolSet.color));
+    this.symbols.forEach((symbolSet) => chartColors.removeColorInUse(symbolSet.color));
     // Reset symbols
     this.symbols = [
       {
@@ -149,7 +149,7 @@ class PortfolioStore {
         currency: "USD",
         performanceSincePortfolioStart: 1,
         annualizedPerformanceSincePortfolioStart: 1,
-        color: this.nextAvailableColorValue(),
+        color: chartColors.nextAvailableColorValue(),
         endValue: 0,
         dateFetched: "-",
       },
@@ -378,11 +378,12 @@ class PortfolioStore {
     this.setTriggerRerenderVisibleLines(true);
     this.setTriggerRerenderPortfolio(true);
   }
-
+}
+const chartColors = {
   nextAvailableColorValue() {
     let availableColorValue = null;
-    for (let index = 0; index < chartColorsForSeries.length; index++) {
-      const element = chartColorsForSeries[index];
+    for (let index = 0; index < this.chartColorsForSeries.length; index++) {
+      const element = this.chartColorsForSeries[index];
       if (!element.isBegingUsed) {
         availableColorValue = element.colorValue;
         element.isBegingUsed = true;
@@ -390,71 +391,70 @@ class PortfolioStore {
       }
     }
     return availableColorValue;
-  }
-
+  },
   removeColorInUse(colorValue) {
-    for (let index = 0; index < chartColorsForSeries.length; index++) {
-      const element = chartColorsForSeries[index];
+    for (let index = 0; index < this.chartColorsForSeries.length; index++) {
+      const element = this.chartColorsForSeries[index];
       if (element.colorValue === colorValue) {
         element.isBegingUsed = false;
         break;
       }
     }
-  }
-}
+  },
+  chartColorsForSeries: [
+    { colorValue: "#3f51b5", isBegingUsed: false },
+    { colorValue: "#2196f3", isBegingUsed: false },
+    { colorValue: "#03a9f4", isBegingUsed: false },
+    { colorValue: "#00bcd4", isBegingUsed: false },
+    { colorValue: "#009688", isBegingUsed: false },
+    { colorValue: "#4caf50", isBegingUsed: false },
+    { colorValue: "#8bc34a", isBegingUsed: false },
+    { colorValue: "#cddc39", isBegingUsed: false },
+    { colorValue: "#ffeb3b", isBegingUsed: false },
+    { colorValue: "#ffc107", isBegingUsed: false },
+    { colorValue: "#ff9800", isBegingUsed: false },
+    { colorValue: "#ff5722", isBegingUsed: false },
+    { colorValue: "#f44336", isBegingUsed: false },
+    { colorValue: "#e91e63", isBegingUsed: false },
+    { colorValue: "#9c27b0", isBegingUsed: false },
+    { colorValue: "#673ab7", isBegingUsed: false },
 
-const chartColorsForSeries = [
-  { colorValue: "#3f51b5", isBegingUsed: false },
-  { colorValue: "#2196f3", isBegingUsed: false },
-  { colorValue: "#03a9f4", isBegingUsed: false },
-  { colorValue: "#00bcd4", isBegingUsed: false },
-  { colorValue: "#009688", isBegingUsed: false },
-  { colorValue: "#4caf50", isBegingUsed: false },
-  { colorValue: "#8bc34a", isBegingUsed: false },
-  { colorValue: "#cddc39", isBegingUsed: false },
-  { colorValue: "#ffeb3b", isBegingUsed: false },
-  { colorValue: "#ffc107", isBegingUsed: false },
-  { colorValue: "#ff9800", isBegingUsed: false },
-  { colorValue: "#ff5722", isBegingUsed: false },
-  { colorValue: "#f44336", isBegingUsed: false },
-  { colorValue: "#e91e63", isBegingUsed: false },
-  { colorValue: "#9c27b0", isBegingUsed: false },
-  { colorValue: "#673ab7", isBegingUsed: false },
+    { colorValue: "#2c387e", isBegingUsed: false },
+    { colorValue: "#1769aa", isBegingUsed: false },
+    { colorValue: "#0276aa", isBegingUsed: false },
+    { colorValue: "#008394", isBegingUsed: false },
+    { colorValue: "#00695f", isBegingUsed: false },
+    { colorValue: "#357a38", isBegingUsed: false },
+    { colorValue: "#618833", isBegingUsed: false },
+    { colorValue: "#8f9a27", isBegingUsed: false },
+    { colorValue: "#b2a429", isBegingUsed: false },
+    { colorValue: "#b28704", isBegingUsed: false },
+    { colorValue: "#b26a00", isBegingUsed: false },
+    { colorValue: "#b23c17", isBegingUsed: false },
+    { colorValue: "#aa2e25", isBegingUsed: false },
+    { colorValue: "#a31545", isBegingUsed: false },
+    { colorValue: "#6d1b7b", isBegingUsed: false },
+    { colorValue: "#482880", isBegingUsed: false },
 
-  { colorValue: "#2c387e", isBegingUsed: false },
-  { colorValue: "#1769aa", isBegingUsed: false },
-  { colorValue: "#0276aa", isBegingUsed: false },
-  { colorValue: "#008394", isBegingUsed: false },
-  { colorValue: "#00695f", isBegingUsed: false },
-  { colorValue: "#357a38", isBegingUsed: false },
-  { colorValue: "#618833", isBegingUsed: false },
-  { colorValue: "#8f9a27", isBegingUsed: false },
-  { colorValue: "#b2a429", isBegingUsed: false },
-  { colorValue: "#b28704", isBegingUsed: false },
-  { colorValue: "#b26a00", isBegingUsed: false },
-  { colorValue: "#b23c17", isBegingUsed: false },
-  { colorValue: "#aa2e25", isBegingUsed: false },
-  { colorValue: "#a31545", isBegingUsed: false },
-  { colorValue: "#6d1b7b", isBegingUsed: false },
-  { colorValue: "#482880", isBegingUsed: false },
+    { colorValue: "#6573c3", isBegingUsed: false },
+    { colorValue: "#4dabf5", isBegingUsed: false },
+    { colorValue: "#35baf6", isBegingUsed: false },
+    { colorValue: "#33c9dc", isBegingUsed: false },
+    { colorValue: "#33ab9f", isBegingUsed: false },
+    { colorValue: "#6fbf73", isBegingUsed: false },
+    { colorValue: "#a2cf6e", isBegingUsed: false },
+    { colorValue: "#d7e360", isBegingUsed: false },
+    { colorValue: "#ffef62", isBegingUsed: false },
+    { colorValue: "#ffcd38", isBegingUsed: false },
+    { colorValue: "#ffac33", isBegingUsed: false },
+    { colorValue: "#ff784e", isBegingUsed: false },
+    { colorValue: "#f6685e", isBegingUsed: false },
+    { colorValue: "#ed4b82", isBegingUsed: false },
+    { colorValue: "#af52bf", isBegingUsed: false },
+    { colorValue: "#8561c5", isBegingUsed: false },
+  ],
+};
 
-  { colorValue: "#6573c3", isBegingUsed: false },
-  { colorValue: "#4dabf5", isBegingUsed: false },
-  { colorValue: "#35baf6", isBegingUsed: false },
-  { colorValue: "#33c9dc", isBegingUsed: false },
-  { colorValue: "#33ab9f", isBegingUsed: false },
-  { colorValue: "#6fbf73", isBegingUsed: false },
-  { colorValue: "#a2cf6e", isBegingUsed: false },
-  { colorValue: "#d7e360", isBegingUsed: false },
-  { colorValue: "#ffef62", isBegingUsed: false },
-  { colorValue: "#ffcd38", isBegingUsed: false },
-  { colorValue: "#ffac33", isBegingUsed: false },
-  { colorValue: "#ff784e", isBegingUsed: false },
-  { colorValue: "#f6685e", isBegingUsed: false },
-  { colorValue: "#ed4b82", isBegingUsed: false },
-  { colorValue: "#af52bf", isBegingUsed: false },
-  { colorValue: "#8561c5", isBegingUsed: false },
-];
 const compareSymbolSetsByTicker = (a, b) => {
   if (a.symbolTicker < b.symbolTicker) {
     return -1;
